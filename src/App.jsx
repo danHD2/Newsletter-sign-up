3fb119a03d65f5d530dbc151631791394039af29
unknown <dan.harabagiu.dimitrescu@gmail.com>
2024-10-21 16:02:27 +0300

    first static build

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='flex items-center justify-center min-h-screen w-full'>
+      <div className='bg-white p-5 rounded-3xl flex flex-row'>
+        <div className='p-10 max-w-md'>
+          <h1 className='text-darkSlateGrey font-semibold'>Stay updated!</h1>
+          <div>
+            <p className='text-darkSlateGrey py-5'>
+              Join 60,000+ product managers receiving monthly updates on:
+            </p>
+            <ul className='text-darkSlateGrey list'>
+              <li className='py-2'>Product discovery and building what matters</li>
+              <li className='py-2'>Measuring to ensure updates are a success</li>
+              <li className='py-2'>And much more!</li>
+            </ul>
+          </div>
+         <form action="submit"className='flex flex-col mt-6'>
+          <label htmlFor="email" className='text-darkSlateGrey font-semibold text-xs'>Email address</label>
+          <input type="text" placeholder='email@company.com' className='text-darkSlateGrey p-3 rounded-md bg-white border mt-2 border-grey' />
+          <button className='p-3 text-white bg-darkSlateGrey hover:bg-tomato transition-all border-0 mt-6'>Subscribe to monthly newsletter</button>
+          </form>       
+        </div>
+        <div>
+          <img src="/src/assets/images/illustration-sign-up-desktop.svg" alt="illustration" />
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    </div>
     </>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,17 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
+      colors: {
+        tomato: "hsl(4, 100%, 67%)",
+        darkSlateGrey: "hsl(234, 29%, 20%)",
+        charcoalGrey: "hsl(235, 18%, 26%)",
+        grey: "hsl(231, 7%, 60%)",
+      },
       fontFamily: {
         custom: ['Roboto Regular', 'sans-serif'], // Add your custom font here
       },
